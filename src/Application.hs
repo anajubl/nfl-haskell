939e3a8e62b8f51404a08e8c384c8a06d8a53fd5
Gustavo Oliveira <gustavo.oliveira141998@hotmail.com>
2019-06-06 06:04:30 +0000

    [ANA] Correções em noticia

--- src/Application.hs
+++ src/Application.hs
@@ -40,6 +40,8 @@ import Handler.Common
 import Handler.Home
 import Handler.Admin
 import Handler.Login
+import Handler.Noticia
+import Handler.Ler
 -- This line actually creates our YesodDispatch instance. It is the second half
 -- of the call to mkYesodData which occurs in Foundation.hs. Please see the
 -- comments there for more details.
