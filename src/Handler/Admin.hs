346152af8c1dcd820942383249179d177641d9cc
Gustavo Oliveira <gustavo.oliveira141998@hotmail.com>
2019-06-06 07:53:14 +0000

    [GUSTAVO e ANA] Update de noticia

--- src/Handler/Admin.hs
+++ src/Handler/Admin.hs
@@ -20,5 +20,34 @@ getAdminR = do
         addStylesheet $ StaticR css_bootstrap_css
         $(whamletFile "templates/admin.hamlet")
         toWidget $(luciusFile "templates/admin.lucius")
+      
+     
+      
+formNoticiaUpdate :: TimeId -> Text -> Text -> Text -> Form Noticia
+formNoticiaUpdate time titulo descricao texto = renderBootstrap $ Noticia
+    <$> areq (selectField listaTimes) "Lista de Times: " (Just time)
+    <*> areq textField "Titulo: " (Just titulo)
+    <*> areq textField "Descrição: " (Just descricao)
+    <*> areq textField "Texto: " (Just texto)
+     
+     
+listaTimes = do
+       entidades <- runDB $ selectList [] [Asc TimeNome] 
+       optionsPairs $ fmap (\ent -> (timeNome $ entityVal ent, entityKey ent)) entidades
+   
         
-        
+getNoticiaUpdateR :: NoticiaId -> Handler Html
+getNoticiaUpdateR noticiaId = do
+
+    (Noticia time titulo descricao texto) <- runDB $ get404 noticiaId
+    ((res, widgetForm ), enctype) <- runFormPost $ formNoticiaUpdate time titulo descricao texto
+    case res of
+        FormMissing -> defaultLayout $(whamletFile "templates/alterarnoticia.hamlet")
+        FormFailure x -> redirect $ NoticiaUpdateR noticiaId
+        FormSuccess noticia -> do 
+            runDB $ do
+                replace noticiaId noticia
+            redirect AdminR
+            
+postNoticiaUpdateR :: NoticiaId -> Handler Html
+postNoticiaUpdateR noticiaId = getNoticiaUpdateR noticiaId
